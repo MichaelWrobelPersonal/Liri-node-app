8bc32c403f007e4cd92c3e6797385d49c1a99a08
MichaelWrobelPersonal <michaelrwrobel@sbcglobal.net>
2018-04-01 22:36:56 -0400

    Added parameter input plus command swith statement

--- liri.js
+++ liri.js
@@ -4,6 +4,21 @@ var keys = require('./keys');
 var request = require('request');
 //request('npm');
 
+// Includes the FS package for reading and writing packages
+const fs = require('fs');
+
+// Run this program by navigating to it in terminal/bash.
+// Then run node liri.js
+console.log("Liri");
+let command = process.argv[2];
+let media = process.argv[3];
+console.log('Command is: '+command);
+console.log('Media is: '+media);
+
+let songFile = 'random.txt';
+console.log('Song File is: '+songFile);
+
+
 // TWITTER
 var Twitter = require('twitter');
 var client = new Twitter( keys.twitter );
@@ -57,3 +72,33 @@ var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey
 // This line is just to help us debug against the actual URL.
 console.log(queryUrl);
 
+
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable "data"
+//fs.readFilreadFilerr, data) {
+fs.readFile(songFile, "utf8", function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+    switch (command)
+    {
+        case 'my-tweets':
+            console.log('my-tweets=tbd');
+        break;
+        case 'spotify-this-song':
+//            fs.appendFile(songFile,media);
+            console.log('spotify-this-song-tbd');
+             break;
+        case 'movie-this':
+//            fs.appendFile(songFile,media);
+            console.log('movie-this-tbd');
+             break;
+        case 'do-what-it-says':
+              console.log('did-what-it-said-tbd');
+              break;
+            break;
+        default:
+            console.log('Try Again')
+    }
+});
